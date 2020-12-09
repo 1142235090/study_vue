1f5eae8bf06db69049fac36f65be7b7ac3f488f7
赵晗 <18833212134@139.com>
2020-12-09 17:15:24 +0800

    lean vue cli

---
+++ .history/vue-cli脚手架/cli2/vuecli2test/src/App_20201208160501.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import banner from './banner/banner';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    banner
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ .history/vue-cli脚手架/cli2/vuecli2test/src/App_20201208160523.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <banner/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import banner from './banner/banner';
+import Banner from './banner/banner.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    banner
+    Banner
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
